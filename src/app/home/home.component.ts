d2a91138697a58dee86db7e305a760517f60c493
T S Ajeet <codewithstatus200@gmail.com>
2024-03-10 16:34:23 +0530

    observables

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -10,5 +11,18 @@ export class HomeComponent {
 
   handleRemove() {
     this.elements.splice(0, 1);
+  }
+
+  ngOnInit() {
+    // const customIntervalObservable = Observable.create(observer => {
+    //   let count = 0;
+    //   setInterval(() => {
+    //     ++count;
+    //     observer.next(count)
+    //     if(count > 3) observer.complete();
+    //   }, 1000)
+    // })
+
+    // customIntervalObservable.pipe(map((data: any) => {return data})).subscribe((res) => {console.log(res)})
   }
 }
